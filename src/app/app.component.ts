927afbed5d463035a2c7e094b3d624b3479b52e6
James Deer (Desktop) <DeveloperJelinek@gmail.com>
2021-10-23 21:00:01 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    all: any;
+
+    constructor(private http: HttpClient) {
+        this.loadData();
+    }
+
+    async loadData() {
+        const data = await this.http.get('https://stocks.jamesdeer.io/dashboard.json').toPromise();
+        this.all = Object.keys(data).map(key => Object.assign({ticker: key}, data[key]));
+    }
+}
